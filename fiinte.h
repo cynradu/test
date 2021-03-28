4d4607bea80dcc11b721c6cb3c875dda098ac9c1
cynradu <63587313+cynradu@users.noreply.github.com>
2021-03-28 20:11:05 +0100

    Add files via upload

---
+++ fiinte.h
@@ -0,0 +1,17 @@
+#include <string>
+
+class fiinte
+{
+public:
+	int age;
+	double height;
+	std::string name;
+	void Enroll_Uni();
+	void IsMale();
+	void IsFemale();
+	fiinte(string name_select; int age_select; double height_select);
+	~fiinte();
+private:
+	std::string gender;
+};
+
